3ade5aff97635c246c4a47c76893a49437fbfe66
Karina Yumi Ferreira <92693545+KarinaYumi@users.noreply.github.com>
2022-04-01 20:18:18 -0300

    Add files via upload

---
+++ POO/Veiculos/Caminhao.Java
@@ -0,0 +1,29 @@
+public class Caminhao{
+    private String modelo, marca;
+    private int eixos;
+
+    public String getModelo(){
+        return modelo;
+    } 
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public int getEixos(){
+        return eixos;
+    }
+
+    public void setEixos(int eixos){
+        this.eixos = eixos;
+    }
+
+}
---
+++ POO/Veiculos/Carro.Java
@@ -0,0 +1,28 @@
+public class Carro{
+    protected String modelo, marca, cor; 
+
+    public String getModelo(){
+        return modelo;
+    } 
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getCor(){
+        return cor;
+    }
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+}
