3404bacca34c9080ab15f74075925a0c70007fb4
Karina Yumi Ferreira <92693545+KarinaYumi@users.noreply.github.com>
2022-04-01 20:26:34 -0300

    Add files via upload

---
+++ POO/Funcionario/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    Funcionario Pedro = new Funcionario(1000.00, "Pedro");
+    Funcionario Vagner = new Funcionario(3500, "Vagner");
+        Pedro.SalarioComissao();
+        Pedro.SalarioLiquido();
+        Vagner.SalarioComissao();
+        Vagner.SalarioLiquido();
+    }
+    } 
