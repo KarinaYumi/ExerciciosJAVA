330a5a35971d57de68034c87aae026b7be93651d
Karina Yumi Ferreira <92693545+KarinaYumi@users.noreply.github.com>
2022-04-01 20:25:56 -0300

    Create Retangulo.java

---
+++ POO/Retangulo/Retangulo.java
@@ -0,0 +1,10 @@
+package ex01;
+
+public class Retangulo {
+
+    public double baseRetangulo, alturaRetangulo;
+    public static void calcularArea(double baseRetangulo, doublealturaRetangulo){
+
+    System.out.println("A AREA DO RETANGULO É IGUAL Á:"+baseRetangulo * alturaRetangulo);
+    }
+}
