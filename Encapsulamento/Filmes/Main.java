8799ed1ab493646c20b8c0da45abdd1c6c8a895b
Karina Yumi Ferreira <92693545+KarinaYumi@users.noreply.github.com>
2022-04-07 08:39:45 -0300

    Update Main.java

--- Encapsulamento/Filmes/Main.java
+++ Encapsulamento/Filmes/Main.java
@@ -9,53 +9,52 @@ public class Main {
 		filme01.setDuracaoemMinutos(142);
 		
 		filme01.exibirDuracaoEmHoras();
-		System.out.println("A duração do filme"+filme01.getTituloFilme()+"em horas é: "+filme01.getDuracaoemHoras()+"h");
+		System.out.println("A duração do filme"+filme01.getTituloFilme()+"em horas é: "+filme01.exibirDuracaoEmHoras());
 		
 		// 2° FILME
 		
 		Filme filme02 = new Filme("Hotel Transilvânia", 93);
 		
 		filme02.exibirDuracaoEmHoras();
-		System.out.println("\nA duração do filme "+filme02.getTituloFilme()+" em horas é: "+filme02.getDuracaoemHoras()+"h");
+		System.out.println("\nA duração do filme "+filme02.getTituloFilme()+" em horas é: "+filme02.exibirDuracaoEmHoras());
 		
 		System.out.println("\nOs filmes em cartaz são: "+filme01.getTituloFilme()+" e "+filme02.getTituloFilme());
 		
-		System.out.println("\n -------------Outros Filmes------------");
+		System.out.println("\n ------------------------Outros Filmes------------------------");
 		
 		// 3° FILME
 		
 		Filme filme03 = new Filme("Velozes e Furiosos ", 180);
 		
 		filme03.exibirDuracaoEmHoras();
-		System.out.println("\nA duração do filme "+filme03.getTituloFilme()+" em horas é: "+filme03.getDuracaoemHoras()+"h");
-		
+		System.out.println("\nA duração do filme "+filme03.getTituloFilme()+" em horas é: "+filme03.exibirDuracaoEmHoras());
 		
 		// 4° FILME
 		
 		Filme filme04 = new Filme("Anny em USA", 120);
 		
 		filme04.exibirDuracaoEmHoras();
-		System.out.println("\nA duração do filme "+filme04.getTituloFilme()+" em horas é: "+filme04.getDuracaoemHoras()+"h");
+		System.out.println("\nA duração do filme "+filme04.getTituloFilme()+" em horas é: "+filme04.exibirDuracaoEmHoras());
 		
 		// 5° FILME
 		
 		Filme filme05 = new Filme("Forest Gump", 200);
 		
 		filme05.exibirDuracaoEmHoras();
-		System.out.println("\nA duração do filme "+filme05.getTituloFilme()+" em horas é: "+filme05.getDuracaoemHoras()+"h");
+		System.out.println("\nA duração do filme "+filme05.getTituloFilme()+" em horas é: "+filme05.exibirDuracaoEmHoras());
 		
 		// 6° FILME
 		
 		Filme filme06 = new Filme("Como eu era Antes de Você", 180);
 		
 		filme06.exibirDuracaoEmHoras();
-		System.out.println("\nA duração do filme "+filme06.getTituloFilme()+" em horas é: "+filme06.getDuracaoemHoras()+"h");
+		System.out.println("\nA duração do filme "+filme06.getTituloFilme()+" em horas é: "+filme06.exibirDuracaoEmHoras());
 		
 		// 7° FILME
 		
 		Filme filme07 = new Filme("A cinco passos de Você", 120);
 		
 		filme07.exibirDuracaoEmHoras();
-		System.out.println("\nA duração do filme "+filme07.getTituloFilme()+" em horas é: "+filme07.getDuracaoemHoras()+"h");
+		System.out.println("\nA duração do filme "+filme07.getTituloFilme()+" em horas é: "+filme07.exibirDuracaoEmHoras());
 	}
 }
