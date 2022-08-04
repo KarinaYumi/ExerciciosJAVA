f74a0d4424d6966cb619d7fd27048875402a5506
Karina Yumi Ferreira <92693545+KarinaYumi@users.noreply.github.com>
2022-08-04 19:32:52 -0300

    Add files via upload

---
+++ Polimorfismo/Animal/cachorro.java
@@ -0,0 +1,17 @@
+public class cachorro extends Animal {
+	
+	@Override
+	public String Correr() {
+		return "O cachorro corre 25km/h";
+	}
+	
+	@Override
+	public String Alimentar() {
+		return "O cachorro se alimenta de ração";
+	}
+	
+	@Override
+	public String EmitirSom() {
+		return "O cachorro faz AUAU";
+	}
+}
---
+++ Polimorfismo/Animal/gato.java
@@ -0,0 +1,18 @@
+public class gato extends Animal{
+	
+	@Override
+	public String Correr() {
+		return "O gato corre 10km/h";
+	}
+	
+	@Override
+	public String Alimentar() {
+		return "O gato se alimenta de ração para gatos";
+	}
+	
+	@Override
+	public String EmitirSom() {
+		return "O gato faz MIAU";
+	}
+	
+}
