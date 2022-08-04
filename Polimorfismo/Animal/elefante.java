e91a5646e453443536128dd174e0781c0526b6ad
Karina Yumi Ferreira <92693545+KarinaYumi@users.noreply.github.com>
2022-08-04 19:42:48 -0300

    Add files via upload

---
+++ Polimorfismo/Animal/elefante.java
@@ -0,0 +1,18 @@
+public class elefante extends Animal {
+	
+	@Override
+	public String Correr() {
+		return "O elefante corre 30km/h";
+	}
+	
+	@Override
+	public String Alimentar() {
+		return "O elefante se alimenta de plantas";
+	}
+	
+	@Override
+	public String EmitirSom() {
+		return "O elefante faz TRUUUUM";
+	}
+	
+}
